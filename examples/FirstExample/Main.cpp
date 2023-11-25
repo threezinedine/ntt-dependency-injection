e3ea2f7d99211c91cf298703f0cb20a247c9481b
threezinedine <threezinedine@gmail.com>
2023-11-26 00:48:37 +0700

    initial setup

---
+++ examples/FirstExample/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <NTTDependencyInjection/NTTDependencyInjection.hpp>
+
+int main(void)
+{
+    std::cout << "Hello Wolrd" << std::endl;
+    return 0;
+}
---
+++ src/InjectionBase.cpp
@@ -0,0 +1,1 @@
+#include <NTTDependencyInjection/InjectionBase.hpp>
